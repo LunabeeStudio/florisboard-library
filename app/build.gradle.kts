f57d3395493e9c8e904783211adbf8797e71eaa6
rlatapy-luna <remi.latapy@lunabee.com>
2023-05-24 11:59:54 +0200

    Add demo app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@
 
 import java.io.ByteArrayOutputStream
 
-val lunabeeVersion = "0.1.0"
+val lunabeeVersion = "0.1.0-6"
 val florisVersion = "0.4.0-beta01"
 val usePrebuilt = true
 version = "$florisVersion-$lunabeeVersion"
---
+++ demo/build.gradle.kts
@@ -0,0 +1,102 @@
+/*
+ * Copyright (C) 2022 Patrick Goldinger
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Suppress needed until https://youtrack.jetbrains.com/issue/KTIJ-19369 is fixed
+@file:Suppress("DSL_SCOPE_VIOLATION")
+
+import java.io.ByteArrayOutputStream
+import java.io.File
+
+plugins {
+    id(libs.plugins.agp.application.get().pluginId)
+    id(libs.plugins.kotlin.android.get().pluginId)
+}
+
+android {
+    namespace = "dev.patrickgold.florisboard"
+    compileSdk = 33
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs = listOf(
+            "-Xallow-result-return-type",
+            "-opt-in=kotlin.contracts.ExperimentalContracts",
+            "-Xjvm-default=all-compatibility",
+        )
+    }
+
+    defaultConfig {
+        applicationId = "dev.patrickgold.florisboard"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 90
+        versionName = "0.4.0"
+
+        buildConfigField("String", "BUILD_COMMIT_HASH", "\"${getGitCommitHash()}\"")
+
+        sourceSets {
+            maybeCreate("main").apply {
+                java {
+                    srcDirs("src/main/kotlin")
+                }
+            }
+        }
+    }
+
+    bundle {
+        language {
+            // We disable language split because FlorisBoard does not use
+            // runtime Google Play Service APIs and thus cannot dynamically
+            // request to download the language resources for a specific locale.
+            enableSplit = false
+        }
+    }
+
+
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+}
+
+dependencies {
+    implementation(project(":florisboard-library"))
+    androidTestImplementation(libs.androidx.test.espresso.core)
+}
+
+fun getGitCommitHash(short: Boolean = false): String {
+    if (!File(".git").exists()) {
+        return "null"
+    }
+
+    val stdout = ByteArrayOutputStream()
+    exec {
+        if (short) {
+            commandLine("git", "rev-parse", "--short", "HEAD")
+        } else {
+            commandLine("git", "rev-parse", "HEAD")
+        }
+        standardOutput = stdout
+    }
+    return stdout.toString().trim()
+}
