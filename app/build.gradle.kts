c86b2c0d890efb996975a15dc34e01f54603049f
rlatapy-luna <remi.latapy@lunabee.com>
2023-06-08 18:18:19 +0200

    Bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@
 
 import java.io.ByteArrayOutputStream
 
-val lunabeeVersion = "0.1.0-10"
+val lunabeeVersion = "0.1.0-11"
 val florisVersion = "0.4.0-beta01"
 val usePrebuilt = true
 version = "$florisVersion-$lunabeeVersion"
