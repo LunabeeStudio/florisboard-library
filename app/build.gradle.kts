2a7c827741b52fe20ae2bef7f3beb38b51ce47dd
Rémi Latapy <45555889+rlatapy-luna@users.noreply.github.com>
2023-07-27 10:24:09 +0200

    Feature/force ime type (#4)
    * open performEnterAction
    
    * bump lb version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@
 
 import java.io.ByteArrayOutputStream
 
-val lunabeeVersion: String = "0.1.15"
+val lunabeeVersion: String = "0.1.16"
 val florisVersion = "0.4.0-beta01"
 val usePrebuilt = true
 version = "$florisVersion-$lunabeeVersion"
