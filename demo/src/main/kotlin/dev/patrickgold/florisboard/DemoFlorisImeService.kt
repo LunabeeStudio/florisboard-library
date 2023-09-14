24100be5d7ad0614d36c1716c2473dbc049479ec
Tom Kubasik <tom.kubasik@lunabee.com>
2023-09-14 10:12:45 +0200

    0.1.17 separate themeView from not themed, match imePadding to keyboard only, add touchache top override

--- demo/src/main/kotlin/dev/patrickgold/florisboard/DemoFlorisImeService.kt
+++ demo/src/main/kotlin/dev/patrickgold/florisboard/DemoFlorisImeService.kt
@@ -1,6 +1,7 @@
 package dev.patrickgold.florisboard
 
 import android.view.KeyEvent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -10,13 +11,16 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.Text
@@ -28,7 +32,9 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
@@ -51,15 +57,13 @@ class DemoFlorisImeService : FlorisImeService() {
     }
 
     @Composable
-    override fun DecoratedIme(ImeUi: @Composable () -> Unit) {
+    override fun AboveImeView(ImeUi: @Composable () -> Unit) {
         var isKeyboardVisible by remember {
             mutableStateOf(true)
         }
-
         Column {
             AboveKeyboardUi(
-                modifier = Modifier
-                    .weight(1f, false),
+                modifier = Modifier.weight(1f, false),
                 isKeyboardVisible = isKeyboardVisible,
                 toggleKeyboardVisibility = { isKeyboardVisible = !isKeyboardVisible },
             )
@@ -67,6 +71,20 @@ class DemoFlorisImeService : FlorisImeService() {
                 ImeUi()
             }
         }
+    }
+
+    @Composable
+    override fun ThemeImeView() {
+        Box(
+            Modifier
+                .background(Color.White)
+                .fillMaxWidth()
+                .height(16.dp)
+        )
+    }
+
+    override fun calculateTouchableTopY(visibleTopY: Int, needAdditionalOverlay: Boolean): Int {
+        return 0
     }
 
     @Composable
@@ -86,88 +104,94 @@ class DemoFlorisImeService : FlorisImeService() {
         val heightModifier by remember(isExpanded) {
             if (isExpanded) {
                 editorInstance.blockInput = true
-                Modifier.fillMaxHeight()
+                Modifier.fillMaxHeight(0.9f)
             } else {
                 editorInstance.blockInput = false
                 Modifier.wrapContentHeight()
             }.let(::mutableStateOf)
         }
-
         Box(
-            modifier
-                .fillMaxWidth()
-                .then(heightModifier)
-                .drawBehind { drawRect(Color(0x80FF0000)) }
-        )
-        {
-            val scrollableState = rememberScrollState()
-
-            Column(
-                verticalArrangement = Arrangement.spacedBy(8.dp),
-                modifier = Modifier
-                    .windowInsetsPadding(WindowInsets.systemBars.only(WindowInsetsSides.Top))
-                    .verticalScroll(scrollableState),
+            modifier = modifier
+                .fillMaxSize()
+                .background(Color(0xFF000000).copy(alpha = 0.5f)),
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            Box(
+                Modifier
+                    .fillMaxWidth()
+                    .then(heightModifier)
+                    .clip(RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp))
+                    .drawBehind { drawRect(Color(0xFFFF0000)) }
             ) {
-                Text("Hello top UI")
-                textFieldValues.forEachIndexed { index, fieldValue ->
-                    key(index) {
-                        TextField(
-                            value = fieldValue,
-                            onValueChange = { newField ->
-                                textFieldValues.removeAt(index)
-                                textFieldValues.add(index, newField)
-                            },
-                            modifier = Modifier
-                                .keyboardTextfield(
-                                    { isKeyboardVisible },
-                                    toggleKeyboardVisibility,
-                                    { textFieldValues[index] }
-                                ) { newField ->
+                val scrollableState = rememberScrollState()
+
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(8.dp),
+                    modifier = Modifier
+                        .windowInsetsPadding(WindowInsets.systemBars.only(WindowInsetsSides.Top))
+                        .verticalScroll(scrollableState),
+                ) {
+                    Text("Hello top UI")
+                    textFieldValues.forEachIndexed { index, fieldValue ->
+                        key(index) {
+                            TextField(
+                                value = fieldValue,
+                                onValueChange = { newField ->
                                     textFieldValues.removeAt(index)
                                     textFieldValues.add(index, newField)
                                 },
-                        )
+                                modifier = Modifier
+                                    .keyboardTextfield(
+                                        { isKeyboardVisible },
+                                        toggleKeyboardVisibility,
+                                        { textFieldValues[index] }
+                                    ) { newField ->
+                                        textFieldValues.removeAt(index)
+                                        textFieldValues.add(index, newField)
+                                    },
+                            )
+                        }
                     }
-                }
-                Row(
-                    modifier = Modifier
-                        .horizontalScroll(rememberScrollState()),
-                    horizontalArrangement = Arrangement.spacedBy(8.dp),
-                ) {
-                    Button(
-                        modifier = Modifier.padding(start = 8.dp),
-                        onClick = {
-                            focusManager.clearFocus()
-                        },
+                    Row(
+                        modifier = Modifier
+                            .horizontalScroll(rememberScrollState()),
+                        horizontalArrangement = Arrangement.spacedBy(8.dp),
                     ) {
-                        Text(text = "Release focus")
+                        Button(
+                            modifier = Modifier.padding(start = 8.dp),
+                            onClick = {
+                                focusManager.clearFocus()
+                            },
+                        ) {
+                            Text(text = "Release focus")
+                        }
+                        Button(onClick = {
+                            isExpanded = !isExpanded
+                        }) {
+                            Text(text = if (isExpanded) "Wrap" else "Full height")
+                        }
+                        Button(onClick = {
+                            toggleKeyboardVisibility()
+                        }) {
+                            Text(text = if (isKeyboardVisible) "Hide keyboard" else "Show keyboard")
+                        }
+                        Button(onClick = {
+                            themeManager.updateActiveTheme(forceNight = true)
+                        }) {
+                            Text(text = "Force night")
+                        }
+                        Button(
+                            modifier = Modifier.padding(end = 8.dp),
+                            onClick = {
+                                themeManager.updateActiveTheme()
+                            },
+                        ) {
+                            Text(text = "Update theme")
+                        }
                     }
-                    Button(onClick = {
-                        isExpanded = !isExpanded
-                    }) {
-                        Text(text = if (isExpanded) "Wrap" else "Full height")
+                    if (!isKeyboardVisible) {
+                        Spacer(modifier = Modifier.windowInsetsPadding(WindowInsets.systemBars.only(WindowInsetsSides.Bottom)))
                     }
-                    Button(onClick = {
-                        toggleKeyboardVisibility()
-                    }) {
-                        Text(text = if (isKeyboardVisible) "Hide keyboard" else "Show keyboard")
-                    }
-                    Button(onClick = {
-                        themeManager.updateActiveTheme(forceNight = true)
-                    }) {
-                        Text(text = "Force night")
-                    }
-                    Button(
-                        modifier = Modifier.padding(end = 8.dp),
-                        onClick = {
-                            themeManager.updateActiveTheme()
-                        },
-                    ) {
-                        Text(text = "Update theme")
-                    }
-                }
-                if (!isKeyboardVisible) {
-                    Spacer(modifier = Modifier.windowInsetsPadding(WindowInsets.systemBars.only(WindowInsetsSides.Bottom)))
                 }
             }
         }
