27afe832ed77faf06c5eb21ecb70692b91d8f652
rlatapy-luna <remi.latapy@lunabee.com>
2025-01-14 12:45:07 +0100

    bump floris

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,5 +1,5 @@
 object Versions {
     private val lunabeeVersion: String = "1.0.2"
-    private val florisVersion: String = "0.4.0"
+    private val florisVersion: String = "0.4.5"
     val fullVersion = "$florisVersion-$lunabeeVersion"
 }
