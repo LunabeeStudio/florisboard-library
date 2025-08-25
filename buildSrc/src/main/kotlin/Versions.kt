e765ad7f28c8681905f7a7f175b76f3f8e7da539
Q2 <quentin.metzler@lunabee.com>
2025-08-25 17:08:51 +0200

    Bump version

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,5 +1,5 @@
 object Versions {
-    private val lunabeeVersion: String = "1.0.2"
-    private val florisVersion: String = "0.4.5"
+    private val lunabeeVersion: String = "1.0.3"
+    private val florisVersion: String = "0.5.0"
     val fullVersion = "$florisVersion-$lunabeeVersion"
 }
