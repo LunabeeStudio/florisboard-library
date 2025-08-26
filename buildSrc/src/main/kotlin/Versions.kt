863ee323b36e61886fafa3a3a9431a1824e77527
Q2 <quentin.metzler@lunabee.com>
2025-08-26 17:47:42 +0200

    rename florisboard settings

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,5 +1,5 @@
 object Versions {
-    private val lunabeeVersion: String = "1.0.4"
+    private val lunabeeVersion: String = "1.0.5"
     private val florisVersion: String = "0.5.0"
     val fullVersion = "$florisVersion-$lunabeeVersion"
 }
